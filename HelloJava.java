4918c410b11b9513556e5e04171847e7e8c59501
NandhaKumarSR <nandhakumarsr@hotmail.com>
2025-01-01 18:19:33 +0530

    feat: Added first Java Program

---
+++ HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+        //First Java code
+        //Pascal Case for Class
+        //Case Sensitive
+        
+        public static void main(String[] args) {
+            System.out.println("Hello Java!!!");   
+        }    
+}
